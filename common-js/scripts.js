9391b9a637c762a8db1317871b49080108faa21b
csellers15 <csellers18@yahoo.com>
2019-09-10 10:56:23 -0400

    initial commit

---
+++ common-js/scripts.js
@@ -0,0 +1,72 @@
+
+(function ($) {
+
+    "use strict";
+	
+	// ACCORDIAN
+	panelAccordian();
+
+	$(window).on('load', function(){
+		
+		// ISOTOPE PORTFOLIO WITH FILTER
+		if(isExists('.portfolioContainer')){
+			var $container = $('.portfolioContainer');
+			$container.isotope({
+				filter: '*',
+				animationOptions: {
+					duration: 750,
+					easing: 'linear',
+					queue: false
+				}
+			});
+		 
+			$('.portfolioFilter a').click(function(){
+				$('.portfolioFilter .current').removeClass('current');
+				$(this).addClass('current');
+		 
+				var selector = $(this).attr('data-filter');
+				$container.isotope({
+					filter: selector,
+					animationOptions: {
+						duration: 750,
+						easing: 'linear',
+						queue: false
+					}
+				 });
+				 return false;
+			}); 
+		}
+	
+	});
+	
+	
+	$('a[href="#"]').on('click', function(event){
+		return;
+	});
+	
+	
+	if ( $.isFunction($.fn.fluidbox) ) {
+		$('a').fluidbox();
+	}
+	
+	
+})(jQuery);
+
+function panelAccordian(){
+	
+	var panelTitle = $('.panel-title');
+	panelTitle.on('click', function(){
+		$('.panel-title').removeClass('active');
+		$(this).toggleClass('active');
+		
+	});
+	
+}
+
+
+function isExists(elem){
+	if ($(elem).length > 0) { 
+		return true;
+	}
+	return false;
+}
