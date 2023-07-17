9dabbd3d07bfc5f3d69aac5b9ccc9c16f0193f5c
Cole Sellers <colesellers0@gmail.com>
2023-07-17 15:15:42 +0000

    updates

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8dbea248c463b02895c7b54bd085414f5ff5923f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8dbea248c463b02895c7b54bd085414f5ff5923f',
+    ),
+  ),
+);
